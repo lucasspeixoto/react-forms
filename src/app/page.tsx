40c7e04735e7a15f59c136d1d14911148bc1798a
Lucas Peixoto <lspeixotodev@gmail.com>
2023-10-05 22:08:01 -0300

    initialize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,167 @@
-import Image from 'next/image'
+"use client";
+
+import { useForm } from "react-hook-form";
+import * as React from "react";
+import { useState } from "react";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+/**
+ * Utilizar o register no lugar do controller, vai permiter
+ * uma maior performance do nosso componente, ja que, com o controller
+ * a cada iteração do usuário com o formulário, o valor novo do formulário
+ * é anotado dentro do estado, gerando mais renderizações.
+ * Com o register, a validação é feita somente no momento do submit.
+ */
+
+//* O superRefine permite validações entre múltiplos campos
+
+const createUserFormSchema = z
+  .object({
+    name: z
+      .string()
+      .nonempty("Nome é obrigatório!")
+      .min(3, "O Nome precisa conter no mínimo 3 caracteres!")
+      .transform((name) => {
+        return name
+          .trim()
+          .split(" ")
+          .map((word) => {
+            return word[0].toLocaleUpperCase().concat(word.substring(1));
+          })
+          .join(" ");
+      }),
+    email: z
+      .string()
+      .nonempty("E-mail é obrigatório!")
+      .email("E-mail em formato inválido!")
+      .toLowerCase()
+      .refine((email) => {
+        return email.endsWith("@gmail.com");
+      }, "O E-mail precisa ser @gmail"),
+    password: z
+      .string()
+      .nonempty("Senha é obrigatório!")
+      .min(6, "A senha precisa conter no mínimo 6 caracteres!"),
+    confirmedPassword: z
+      .string()
+      .nonempty("A confirmação da senha é obrigatório!")
+      .min(6, "A senha precisa conter no mínimo 6 caracteres!"),
+  })
+  .refine((data) => data.password === data.confirmedPassword, {
+    message: "As senhas precisam ser iguais",
+    path: ["confirmedPassword"], // path of error
+  });
+
+type CreateUserFormData = z.infer<typeof createUserFormSchema>;
 
 export default function Home() {
+  const [formOutput, setFormOutput] = useState<string>("");
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CreateUserFormData>({
+    resolver: zodResolver(createUserFormSchema),
+  });
+
+  function createUserHandler(data: unknown) {
+    setFormOutput(JSON.stringify(data, null, 2));
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <React.Fragment>
+      <div className="h-auto w-auto bg-zinc-700 rounded-lg p-8 flex items-center justify-center">
+        <form
+          onSubmit={handleSubmit(createUserHandler)}
+          className="flex flex-col gap-4"
+        >
+          <div className="flex flex-col gap-2">
+            <label htmlFor="name" className="text-white font-bold">
+              Nome
+            </label>
+            <input
+              type="text"
+              className="rounded-lg border-4 border-blue-300 border-stroke bg-transparent py-2 pl-6 pr-10 outline-none focus:border-blue-500 focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input"
+              {...register("name")}
             />
-          </a>
-        </div>
+            <>
+              {errors.name && (
+                <span className="text-xs text-red-400">
+                  {errors.name.message}
+                </span>
+              )}
+            </>
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <label htmlFor="email" className="text-white font-bold">
+              E-mail
+            </label>
+            <input
+              type="email"
+              className="rounded-lg border-4 border-blue-300 border-stroke bg-transparent py-2 pl-6 pr-10 outline-none focus:border-blue-500 focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input"
+              {...register("email")}
+            />
+            <>
+              {errors.email && (
+                <span className="text-xs text-red-400">
+                  {errors.email.message}
+                </span>
+              )}
+            </>
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <label htmlFor="password" className="text-white font-bold">
+              Senha
+            </label>
+            <input
+              type="password"
+              {...register("password")}
+              className="rounded-lg border-4 border-blue-300 border-stroke bg-transparent py-2 pl-6 pr-10 outline-none focus:border-blue-500 focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input"
+            />
+            <>
+              {errors.password && (
+                <span className="text-xs text-red-400">
+                  {errors.password.message}
+                </span>
+              )}
+            </>
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <label htmlFor="password" className="text-white font-bold">
+              Confirmar Senha
+            </label>
+            <input
+              type="password"
+              {...register("confirmedPassword")}
+              className="rounded-lg border-4 border-blue-300 border-stroke bg-transparent py-2 pl-6 pr-10 outline-none focus:border-blue-500 focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input"
+            />
+            <>
+              {errors.confirmedPassword && (
+                <span className="text-xs text-red-400">
+                  {errors.confirmedPassword.message}
+                </span>
+              )}
+            </>
+          </div>
+
+          <div className="flex justify-center">
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 p-2 w-fit rounded-lg font-semibold text-white h-10"
+            >
+              Entrar
+            </button>
+          </div>
+        </form>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <pre className="m-4">{formOutput}</pre>
+    </React.Fragment>
+  );
 }
